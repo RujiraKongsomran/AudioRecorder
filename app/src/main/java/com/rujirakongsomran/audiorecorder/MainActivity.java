9e4b429949a0629744e68a400517ed3a769b9681
Rujira Kongsomran <biwberry@outlook.com>
2020-05-16 11:33:26 +0700

    Request Permission and initial view

--- app/src/main/java/com/rujirakongsomran/audiorecorder/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/audiorecorder/MainActivity.java
@@ -1,14 +1,79 @@
 package com.rujirakongsomran.audiorecorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ornach.nobobutton.NoboButton;
 
 public class MainActivity extends AppCompatActivity {
+    NoboButton btnStartRecord;
+    NoboButton btnStopRecord;
+    NoboButton btnStartPlay;
+    NoboButton btnStopPlay;
+    String pathSave = "";
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+
+    final int REQUEST_PERMISSION_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+
+        // From Android M, you need request Run-time permission
+        if (checkPermissionFromDevice()) {
+
+
+        } else {
+            requestPermission();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO
+        }, REQUEST_PERMISSION_CODE);
+    }
+
+    private boolean checkPermissionFromDevice() {
+        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
+                record_audio_result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void initInstances() {
+        btnStartRecord = (NoboButton) findViewById(R.id.btnStartRecord);
+        btnStopRecord = (NoboButton) findViewById(R.id.btnStopRecord);
+        btnStartPlay = (NoboButton) findViewById(R.id.btnStartPlay);
+        btnStopPlay = (NoboButton) findViewById(R.id.btnStopPlay);
     }
 }
